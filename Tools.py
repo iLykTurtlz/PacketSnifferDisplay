eba12c11693d4ce957dd54305de09450cb0046ec
nickelm42 <nicolasloayza@gmail.com>
2022-12-03 15:38:21 +0100

    Fixed str to bin coversion in Tools.py

--- Tools.py
+++ Tools.py
@@ -5,3 +5,16 @@ def str_to_hex(s):
 	
 def str_to_int(s):
 	return int(s,16)
+
+# beacuse hex in python is a srting
+def str_to_bin(s):
+    print(s)
+    
+    res = ""
+    for i in range (len(s)):
+        tmp = (int(s[i], 16))
+        tmp = (bin(tmp))[2:]
+        while(len(tmp) < 4):
+            tmp="0"+tmp
+        res = res+tmp
+    return res
